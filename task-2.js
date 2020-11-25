9b6a13dca93b52fc0a61ddd14a07ba365bdccb87
AlexVV82 <67260265+AlexVV82@users.noreply.github.com>
2020-11-25 23:34:41 +0200

    Update task-2.js

--- task-2.js
+++ task-2.js
@@ -1,15 +1,21 @@
 const ingredients = [
-    'Картошка',
-    'Грибы',
-    'Чеснок',
-    'Помидоры',
-    'Зелень',
-    'Приправы',
-  ];
-  
-  const allproducts = ingredients.map(item => {
-    const li = document.createElement('li');
-    li.textContent = item;
-    return li;
-  });
-  document.querySelector('#ingredients').prepend(...allproducts);
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы",
+];
+
+// 1 method кращий  //
+
+const ingList = document.getElementById("ingredients");
+
+const ingredientsItems = (el) => {
+  const itemRef = document.createElement("li");
+  itemRef.textContent = el;
+  return itemRef;
+};
+
+const list = ingredients.map((item) => ingredientsItems(item));
+ingList.append(...list);
