2cd0a4cf115a85094c707aa66e3c50134fdcb87f
AlexVV82 <67260265+AlexVV82@users.noreply.github.com>
2020-11-27 12:49:43 +0200

    Update task-2.js

--- task-2.js
+++ task-2.js
@@ -1,13 +1,7 @@
-const ingredients = [
-  "Картошка",
-  "Грибы",
-  "Чеснок",
-  "Помидоры",
-  "Зелень",
-  "Приправы",
-];
+const ingredients = ["Картошка", "Грибы", "Чеснок", "Помидоры", "Зелень", "Приправы"];
 
 const listEl = document.querySelector("#ingredients");
+//через ф-цию
 const makeListItems = (ingredients) => {
   return ingredients.map((ingredient) => {
     const listItemEl = document.createElement("li");
